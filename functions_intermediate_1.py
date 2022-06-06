65c3e19ce38530f6b5d6e92e2332d3814082a637
Stef Chateaubriand <103604314+Stef-Chateaubriand@users.noreply.github.com>
2022-06-06 09:26:07 -0400

    Add files via upload

---
+++ functions_intermediate_1.py
@@ -0,0 +1,106 @@
+
+#1
+
+'''x = [ [5,2,3], [10,8,9] ]
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+
+students[0]['first_name'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+z[0]['y'] = 30
+print(z)
+'''
+
+#2
+
+'''def interateDictionary(some_list: list):
+    for i in some_list:
+        if i:
+            sorted_keys = sorted(list(i.keys()))
+            pairs = [f"{k} - {i[k]}" for k in sorted_keys]
+            s = ", ".join(pairs)
+            print(s)
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    
+    #-----dont know-----
+    ]'''
+
+
+
+#3
+'''
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary2():
+    for person in students:
+        print(person['first_name'], end="\n")
+    print()
+iterateDictionary2()'''
+
+#4
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+LOCATIONS
+San
+Jose
+Seattle
+Dallas
+Chicago
+Tulsa
+DC
+Burbank
+
+INSTRUCTORS
+Michael
+Amy
+Eduardo
+Josh
+Graham
+Patrick
+Minh
+Devoncopy
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
+
+dont know
+
+
+
+
+
+
+
+
+
+
